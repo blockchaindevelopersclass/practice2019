f174f95b081e15b390426f01c2dfdbe64d73627a
catena <catena@protonmail.com>
2019-02-11 11:46:49 +0300

    signature forger test

---
+++ src/main/scala/crypto/Curve25519SignatureForger.scala
@@ -0,0 +1,12 @@
+package crypto
+
+import scorex.crypto.signatures.Signature
+
+object Curve25519SignatureForger {
+
+  private val modifier: BigInt = BigInt("7237005577332262213973186563042994240829374041602535252466099000494570602496") +
+    BigInt("27742317777372353535851937790883648493")
+
+  def forgeSignature(signature: Signature): Signature = ???
+
+}
